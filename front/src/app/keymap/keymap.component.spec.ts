4e917688110ca0061bbae62302c2feff13f6f3ab
Juan Aguillon <miloca.98@hotmail.com>
2018-09-15 12:30:21 -0500

    Integracion Angular Front-end

---
+++ front/src/app/keymap/keymap.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { KeymapComponent } from './keymap.component';
+
+describe('KeymapComponent', () => {
+  let component: KeymapComponent;
+  let fixture: ComponentFixture<KeymapComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ KeymapComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(KeymapComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
